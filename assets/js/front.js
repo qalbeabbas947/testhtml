5e4cb21d03943b68ef91e234436ea2f19658d22f
qalbeabbas947 <coordinator947@gmail.com>
2025-07-26 03:12:58 +0500

    Fixed the responsiveness

--- assets/js/front.js
+++ assets/js/front.js
@@ -1,25 +1,4 @@
 $(function()
 {  
-    //value for animation  
-    var width = 900;  
-    var animationSpeed = 1000;  
-    var pause = 3000;  
-    var currentSlide = 1;  
-    //Dom element.   
-    var $slider = $('#slider');  
-    var $sliderAnimation = $slider.find('.slides');  
-    console.log($sliderAnimation)
-    var $slides = $sliderAnimation.find('.slide');  
-  
-    setInterval(function()
-    {  
-         $sliderAnimation.animate({'margin-left': '-='+width}, animationSpeed, function(){  
-         currentSlide ++;  
-         if(currentSlide === $slides.length)  
-         {  
-                $sliderAnimation.css('margin-left', 0);  
-                currentSlide = 1;  
-         }  
-     });  
-    },pause);  
+     
 });  
